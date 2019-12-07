47fe84c1f97d88232711dbd60c4fc9416036cf4e
fileaccent <x863557386@163.com>
2019-12-07 17:40:11 +0800

    电脑没电了,把写的先push上去

---
+++ pages/coach/coachSearch/coachCardItem/coachCardItem.js
@@ -0,0 +1,30 @@
+// pages/coach/coachSearch/coachCardItem/coachCardItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    falseData: {
+      type: Object,
+      value: {
+        imgUrl: "http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=1188387633,958216909&os=180840287,211810796&simid=3483313748,650657841&pn=0&rn=1&di=164450&ln=1106&fr=&fmq=1575711408921_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201507%2F04%2F20150704212949_PSfNZ.jpeg&rpstart=0&rpnum=0&adpicid=0&force=undefined",
+        name: "姓名",
+        team: "无"
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
